203420bbca52d79d90ede0af779fce344012ccea
paco <francisco@formula-web.es>
2022-07-04 14:16:52 +0200

    modulo 5.8 styled components

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,20 @@
+// COMPONENTE Home  (=para pagina home)
+// Usa StIled Componentes (CSS en JS)
 import React from "react";
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import {Link, useNavigate} from 'react-router-dom';
 import AddTodo from "./tareas/containers/AddTodo";
 import VisibleTodoList from "./tareas/containers/VisibleTodoList";
 import {useDispatch } from 'react-redux';
 import Footer from "./tareas/Footer";
 
-
+//estilos reutilizables
+let botonenviar = css`
+  padding: 20em;
+  border: solid gray;
+  border-radius: 4px;
+  font-weight: bold;
+`
 
 // Componente React Boton =  button stilizado con styled css 
 export const Boton = styled.button`
@@ -14,6 +22,7 @@ export const Boton = styled.button`
   display: inline-block;
   background-color: #e10098;
   color: ${(props)=>props.color? props.color: '#fff'  }; 
+  ${botonenviar}
 `;
 export const Boton2 = styled(Boton)`
   padding: 10px;
@@ -21,11 +30,28 @@ export const Boton2 = styled(Boton)`
 ` ;
 
 const Container = styled.div`
-  padding: 10px;
+  padding: 100px;
   background-color: #0ac1a8;
+  & a {
+    color: red;
+  }
+  &:hover { background: gray}
+  &.clase-importante { background: yellow}
+  .clase-importante & { background: black } //si el padre tiene la clase dada
+  & > div { background: white} // div hijo directo
 `;
 
-
+const Container2 = styled.input.attrs( (props)=>{
+  return {
+    type: 'email',
+    placeholder:'tu email'
+    
+  }
+})`
+  border: 2px solid black;
+`
+  
+  
 
 
 
@@ -47,6 +73,9 @@ let Home = () => {
           <p><Link to='/usuarios/login'>Link a /usuarios/login</Link></p>
           <p><Link to='/usuarios/miperfil'>Mi Perfil</Link></p>
           <Boton2 id='boton' color='#f9f929' onClick={ ()=>navegar('/usuarios/miperfil')}>Mi Perfil</Boton2>
+          <Container><a>Enlace color rojo</a> </Container>
+         
+         
 
         </div>
         <div id='div2'>

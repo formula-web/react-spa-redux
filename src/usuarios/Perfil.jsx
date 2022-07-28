ed29277aeae6fe920eb115cd411f3a2664698850
paco <francisco@formula-web.es>
2022-07-28 13:13:35 +0200

    Modulo 5.17 a medias

--- src/usuarios/Perfil.jsx
+++ src/usuarios/Perfil.jsx
@@ -1,8 +1,40 @@
 import React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
+import styled from "styled-components";
 import { loadVideosUser } from "../store/videos";
+import { SmallContainer } from "../theme/LayoutApp";
 import Videoplayer from "../videos/Videoplayer";
+
+let PerfilHeader = styled.header`
+display: grid;
+
+grid-template-columns: repeat(6, minmax(auto,1fr));
+grid-template-rows: 200px;
+grid-template-areas: "imagen   imagen   imagen   imagen   imagen   imagen"
+                     "userinfo userinfo userinfo userinfo userinfo userinfo";
+                   
+text-align: center;
+& .imagencont {
+    grid-area: imagen;
+
+}
+& .userinfocont {
+    grid-area: userinfo;
+}
+    
+`;
+
+let PerfilImagen = styled.img`
+  max-height  : 20vh;
+  border-radius: 50%;
+`;
+
+//Este styled div recibe un argumento grid area (en los atributos html de la llamada)
+let Contador = styled.div`
+    grid-area: ${ ( {gridarea})=>gridarea };
+    
+`;
 
 let Perfil = ()=>{
     let dispatcher = useDispatch();
@@ -16,8 +48,19 @@ let Perfil = ()=>{
     ,[])
 
     return (
-        <div>
-            <h1>Perfil de Usuario</h1>
+        <>
+
+ 
+        <SmallContainer>
+        <PerfilHeader>       
+            <div className="imagencont">
+                <PerfilImagen src="/huevos.jpg" />
+            </div>
+            <div className="userinfocont">
+                <Contador>{videos.length} Videos subidos</Contador>
+            </div>
+
+
             <div>{
                 videos.map( (video,index)=>
                     (
@@ -28,8 +71,9 @@ let Perfil = ()=>{
                     )
                 )
             }</div>
-
-        </div>
+        </PerfilHeader>
+        </SmallContainer>
+        </>
     )
 }
 export default Perfil;

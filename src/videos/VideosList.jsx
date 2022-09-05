10fe7157b1eed7e311312254b8d18b8091821d07
Paco <francisco@formula-web.es>
2022-09-05 19:11:22 +0200

    Scroll infinito terminado

--- src/videos/VideosList.jsx
+++ src/videos/VideosList.jsx
@@ -10,6 +10,8 @@ import React from "react";
 import Video from "./Video";
 import {
     AutoSizer,
+    CellMeasurer,
+    CellMeasurerCache,
     InfiniteLoader, List
 } from "react-virtualized"
 
@@ -20,33 +22,38 @@ let rowRenderer = ({index, key, style, parent})=>{
 
 // Componente VideoList ----------------------------------
 let VideosList = ( {videosState, loadNextPage} )=>{
-    
+   const cache = React.useRef(
+    new CellMeasurerCache({fixedWidth:true, defaultHeight:100})
+   ); 
    let isRowLoaded = ({index})=>{   // true = elemento/fila está ya cargado y se puede mostrar
         return !!videosState.data.videos[index];  //true si videos[index] no es null
    };  
    let loadMoreRows = ()=>{ loadNextPage() };  // funcion que carga mas elementos/filas ?
    let xx =  ({onRowsRendered, registerChild})=>(
-    <AutoSizer>
-    { ( {width, height})=>(
-        <List
-        onRowsRendered={onRowsRendered}
-        ref = {registerChild}
-        height = {500}
-        width={700}
-        rowHeight={100}
-        rowCount = {4}
-        rowRenderer = { ({index, key, style, parent})=>{
-            console.log("--rowRenderer--index:",index," video:",videosState.data.videos[index].title);
-            let video = videosState.data.videos[index];
-            return (<div key={index} style={style}>
-            <Video video={video} key={video.id} />
-            </div>);
-        } }
-    >
-    </List>
+    <div style={{width:"200px", height:"80vh"}}>
+        <AutoSizer>
+        { ( {width, height})=>(
+            <List
+            onRowsRendered={onRowsRendered}
+            ref = {registerChild}
+            height = {height}
+            width={width}
+            rowHeight={100}
+            rowCount = {4}
+            rowRenderer = { ({index, key, style, parent})=>{
+                console.log("--rowRenderer--index:",index," video:",videosState.data.videos[index].title);
+                let video = videosState.data.videos[index];
+                return (<div key={index} style={style}>
+                <Video video={video} key={video.id} />
+                </div>);
+            } }
+        >
+        </List>
 
-    )}
-    </AutoSizer>
+        )}
+        </AutoSizer>
+
+    </div>
 
 );
 
@@ -54,27 +61,40 @@ let VideosList = ( {videosState, loadNextPage} )=>{
         <div>
     
             <h2>Listado scroll infinito</h2>
-            <AutoSizer>
-            { ({width, height})=>(
-                <List          
-                    height = {height}
-                    width={width}
-                    rowHeight={50}
-                    rowCount = {10}
-                    /* Renderiza item video de posicion index del state de videos: */
-                    rowRenderer = { ({index, key, style, parent})=>{ 
-                        let video = videosState.data.videos[index];
-                        return (
-                        <div key={index} style={style}>
-                            {video?<Video video={video} key={key} />:<p>no video</p>}
-                            Titulo: { videosState.data.videos[index]? videosState.data.videos[index].title : "--sin titulo--"}
-                        </div>);
-                    } }
-                    >
-                </List>
+            <div style={{width:"200px", height:"80vh"}}>
+                <AutoSizer>
+                { ({width, height})=>(
+                    <List          
+                        height = {height}
+                        width={width}
+                        rowHeight={cache.current.rowHeight}
+                        deferredMeasurementCache={cache.current}
+                        rowCount = {10}
+                        /* Renderiza item video de posicion index del state de videos: */
+                        rowRenderer = { ({index, key, style, parent})=>{ 
+                            let video = videosState.data.videos[index];
+                            return (
+                            <CellMeasurer
+                                key={key}
+                                cache={cache.current}
+                                parent={parent}
+                                columnIndex={0}
+                                rowIndex={index}
+                            >
+                                <div key={index} style={style}>
+                                    {video?<Video video={video} key={key} />:<p>no video</p>}
+                                    Titulo: { videosState.data.videos[index]? videosState.data.videos[index].title : "--sin titulo--"}
+                                </div>
+                            </CellMeasurer>
+                            );
+                        } }
+                        >
+                    </List>
 
-            )}
-            </AutoSizer>
+                )}
+                </AutoSizer>
+            </div>
+
         </div>
 
     )
